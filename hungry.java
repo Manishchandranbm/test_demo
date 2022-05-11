9b45c087ea401d4d8a2b9a88c9086741c741c509
Manishchandranbm <105355692+Manishchandranbm@users.noreply.github.com>
2022-05-11 20:39:40 +0530

    Update hungry.java

--- hungry.java
+++ hungry.java
@@ -5,6 +5,7 @@ public class hungry {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 hungry h1  = new hungry();
+		system.out.println("not hungry")
 System.out.println("hungry");
 	}
 
