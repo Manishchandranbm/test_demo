8482ad16b28b8980902993c7402bf611079f8b88
Your Name <you@example.com>
2022-05-11 20:34:07 +0530

    first version of hungry code

---
+++ hungry.java
@@ -0,0 +1,11 @@
+package manishc;
+
+public class hungry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+hungry h1  = new hungry();
+System.out.println("hungry");
+	}
+
+}
